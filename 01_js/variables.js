5f7138afd3910d009f62bc529b56ad2de0d1d646
CHIRANJEEV PRAJAPAT <73808605+prajapatchiranjeev@users.noreply.github.com>
2024-01-05 12:49:46 +0000

    variables config done

---
+++ 01_js/variables.js
@@ -0,0 +1,11 @@
+const accountID = 129556
+let accountEmail = "chiranjeev@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountID = 3 // not allowed 
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
